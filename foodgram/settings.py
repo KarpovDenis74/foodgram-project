4383992c8213b3ee56c111d7a9280823cc04e1b6
KarpovDenis74 <fotokarpov@yandex.ru>
2021-07-09 17:17:34 +0300

    Create nginx/default.conf

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['130.193.41.184',
+ALLOWED_HOSTS = ['*',
+                 '130.193.41.184',
                  'localhost',
                  '127.0.0.1',
                  "testserver", ]
