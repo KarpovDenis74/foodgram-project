16241131c481772d75bc44ce546f2590c91f655c
KarpovDenis74 <fotokarpov@yandex.ru>
2021-07-15 14:49:49 +0300

    Add indexNotAuth.htm and formRecipe.html

--- recipes/views.py
+++ recipes/views.py
@@ -19,7 +19,7 @@ class RecipeView:
     @login_required
     def new(request):
         context = {'title': 'Создание рецепта',
-                   'button_name': 'Сохранить'}
+                   'button_name': 'Создать рецепт'}
         form = RecipeForm(request.POST or None,
                           files=request.FILES or None)
         if not form.is_valid():
