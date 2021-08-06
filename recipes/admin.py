0e0b573d939e9b449b926b32f1f3159b96fd8777
karpovdenis <karpovdenis@iMac-Denis.local>
2021-08-06 21:11:54 +0300

    Add Favorite model in admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from recipes.models import (Ingredient, Recipe,
                             RecipeIngredient, MealTime,
-                            RecipeMealTime)
+                            RecipeMealTime, Favorite)
 
 
 class MembershipInline(admin.TabularInline):
@@ -43,7 +43,15 @@ class MealTimeAdmin(admin.ModelAdmin):
     empty_value_display = "-пусто-"
 
 
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ("pk", "user", "recipe")
+    search_fields = ("user",)
+    list_filter = ("recipe",)
+    empty_value_display = "-пусто-"
+
+
 admin.site.register(MealTime, MealTimeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
+admin.site.register(Favorite, FavoriteAdmin)
